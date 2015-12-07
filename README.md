c82c5235102944d70135d620b0ea1b67b2f930c9
John Perry <jgperry@ajc.com>
2015-12-07 14:15:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# RtfParse
+A ruby miodule wrapping the GNU copmmand-line utility unrtf
+
+## Dependencies
+- [unrtf](https://www.gnu.org/software/unrtf/)
+- [OPen4](https://github.com/ahoward/open4)
+
+## Usage
+`require 'path/to/rtf_parse.rb'`
+`RtfParse.to_text(rtf_text_or_file_path)`
+`RtfParse.to_html(rtf_text_or_file_path)`
+To use a custom config file:
+`RtfParse.custom_parse(rtf_text_or_file_path, config_path: 'path/to/config/files/', tags_file: 'custom_config_file_name')`
---
+++ lib/rtf_parse.rb
@@ -0,0 +1,49 @@
+require "open4"
+
+module RtfParse
+  attr_accessor(:text)
+  def self._parse(rtf, command)
+    Open4::popen4(command) do |pid, stdin, stdout, stderr|
+      stdin.puts rtf
+      stdin.close
+      @text = stdout.read.strip
+    end
+    return @text
+  end
+
+  def self.to_text(source)
+    config_path = File.expand_path(File.dirname(__FILE__) + '/../config')
+    command = "unrtf -P #{config_path} -t text"
+    rtf =  source =~ /^{\\rtf/ ?
+      source :
+      File.open(source, 'r').read
+
+    return _parse(rtf, command)
+  end
+
+  def self.to_html(source)
+    config_path = File.expand_path(File.dirname(__FILE__) + '/../config')
+    command = "unrtf -P #{config_path} -t html"
+    rtf =  source =~ /^{\\rtf/ ?
+      source :
+      File.open(source, 'r').read
+
+    return _parse(rtf, command)
+  end
+
+  def self.custom_parse(source, options = {})
+    p options
+    config_path = options[:config_path] || File.expand_path(File.dirname(__FILE__) + '/../config')
+    tags_file = options[:tags_file] || 'text'
+    command = "unrtf -P #{config_path} -t #{tags_file}"
+    rtf =  source =~ /^{\\rtf/ ?
+      source :
+      File.open(source, 'r').read
+
+    return _parse(rtf, command)
+  end
+
+  class << self
+    private :_parse
+  end
+end
